e4830e2168107bdb89c78631b01f22aacb81c2d5
maseterpopy <ilovengo@qq.com>
2020-06-04 14:19:26 +0800

    1

--- localServer/src/main/java/personal/popy/localserver/connect/nio/NioSelector.java
+++ localServer/src/main/java/personal/popy/localserver/connect/nio/NioSelector.java
@@ -6,11 +6,16 @@ import java.nio.channels.Selector;
 import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
 import java.util.Set;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 
 public class NioSelector implements NioHandler {
     private Selector readOrAccept;
     private ServerSocketChannel serverChannel;
 
+    private Executor[] singles;
+
+    private int cnt;
 
     public void init() {
         try {
@@ -18,7 +23,10 @@ public class NioSelector implements NioHandler {
             serverChannel = ServerSocketChannel.open();
             serverChannel.configureBlocking(false);
             serverChannel.register(readOrAccept, SelectionKey.OP_ACCEPT, this);
-
+            singles = new Executor[16];
+            for (int i = 0; i < 16; i++) {
+                singles[i] = Executors.newSingleThreadExecutor();
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -41,7 +49,9 @@ public class NioSelector implements NioHandler {
         try {
             SocketChannel accept = serverChannel.accept();
             accept.configureBlocking(false);
-            accept.register(readOrAccept, SelectionKey.OP_READ, new NioReader(accept));
+            NioDispacher att = new NioDispacher(accept);
+            att.setExecutor(singles[cnt++ & 15]);
+            accept.register(readOrAccept, SelectionKey.OP_READ, att);
         } catch (IOException e) {
             e.printStackTrace();
         }
---
+++ localServer/src/test/java/SyncArray.java
@@ -0,0 +1,13 @@
+import java.util.concurrent.locks.ReentrantLock;
+
+public class SyncArray {
+
+    private int state;
+
+    private int expect;
+
+    private ReentrantLock lock;
+
+
+
+}
